9d505d6f788de071000aa3d4c419af51f9a134a3
Ashish <ASHISH-VASAVA@users.noreply.github.com>
2025-07-22 15:09:38 +0530

    change

--- dashboard/src/components/Menu.js
+++ dashboard/src/components/Menu.js
@@ -19,7 +19,7 @@ const Menu = () => {
 
  const handleLogout = () => {
   localStorage.removeItem("username");
-  window.location.href = "https://zerodhaclonedashboard.onrender.com/"; // Redirect to home page after logout
+  window.location.href = "https://zerodhaclonefrontend-t4pc.onrender.com"; // Redirect to home page after logout
 };
 
 
