4691ba6057f9ef15831550e9357d7f7dd6c1820b
Ashish <ASHISH-VASAVA@users.noreply.github.com>
2025-07-23 13:05:17 +0530

    change

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./OrdersList.css";
+import "./Order.css";
 
 const OrdersList = () => {
   const [orders, setOrders] = useState([]);
